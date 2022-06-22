6f16d04e283b6dda85c3ec9c90a678805a31e53d
Yaroslav <crosskyiv@gmail.com>
2022-06-22 08:27:17 +0300

    objects/ prototypes

---
+++ Prototype/prototype.js
@@ -0,0 +1,16 @@
+// 01
+//const personPrototype = {
+//	greet(){
+//		console.log(`hello, my name is ${this.name}!`);
+//	}
+//}
+//function Person(name){
+//	this.name = name
+//}
+//Person.prototype = personPrototype
+//Person.prototype.constructor = Person
+
+//const ted = new Person('Ted')
+//ted.greet()
+//console.log(ted);
+
